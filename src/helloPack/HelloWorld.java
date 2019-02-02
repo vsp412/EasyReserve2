e846fcd86f306a709ed8d991fa5ef4075d1b9511
Pande <pandev@student.douglascollege.ca>
2019-02-01 16:37:55 -0800

    Converted HelloWorld

--- src/helloPack/HelloWorld.java
+++ src/helloPack/HelloWorld.java
@@ -1,11 +1,25 @@
 package helloPack;
+import java.awt.Font;
 
-public class HelloWorld {
+import javax.swing.*;
 
+
+public class HelloWorld extends JFrame{
+
+	
 	public static void main(String[] args) {
 		
+        JFrame jf = new JFrame("Hello World GUI");
 
-
+        jf.setVisible(true);
+        jf.setSize(400, 400);
+        JLabel jl = new JLabel("EasyReserve");
+      
+        
+       Font labelFont = jl.getFont();
+       jl.setFont(new Font("Courier New", Font.PLAIN, 35));
+        jf.add(jl);
+        
 		System.out.println("Hello World Vineet !");
 
 
