d93f5b201eccad97bf075b8722dd14983c91a345
Tiwana <tiwanak@student.douglascollege.ca>
2019-02-01 16:11:55 -0800

    3rd commit

--- src/helloPack/HelloWorld.java
+++ src/helloPack/HelloWorld.java
@@ -4,11 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
-		System.out.println("Hello World, This is Karandeep Tiwana!");
-=======
+
+
 		System.out.println("Hello World Vineet !");
->>>>>>> 82b1c1645b2aada438f606b5fd658ceefb724536
+
 		
 
 	}
