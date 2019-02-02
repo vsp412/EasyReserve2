e3018f7d9899c8a28f37abcff92ecdbd955b4ea4
Pande <pandev@student.douglascollege.ca>
2019-02-01 16:59:53 -0800

    Added user 0 to hello  world app

--- src/helloPack/HelloWorld.java
+++ src/helloPack/HelloWorld.java
@@ -13,13 +13,13 @@ public class HelloWorld extends JFrame{
 
         jf.setVisible(true);
         jf.setSize(400, 400);
-        JLabel jl = new JLabel("EasyReserve");
+        JLabel jl = new JLabel("EasyReserveVineet");
       
-        
+       
        Font labelFont = jl.getFont();
        jl.setFont(new Font("Courier New", Font.PLAIN, 35));
         jf.add(jl);
-        
+     
 		System.out.println("Hello World Vineet !");
 
 
